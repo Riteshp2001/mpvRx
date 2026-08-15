d8b6d7a59bbe7243e47d289c0b86ced23189b7ef
Utsav <utsavchhapra516@gmail.com>
2026-08-15 22:41:47 +0530

    fix: dedupe theme_aurora strings, add celestial effects toggle

--- app/src/main/java/app/gyrolet/mpvrx/preferences/AppearancePreferences.kt
+++ app/src/main/java/app/gyrolet/mpvrx/preferences/AppearancePreferences.kt
@@ -61,6 +61,7 @@ class AppearancePreferences(
   val showJellyfinTab = preferenceStore.getBoolean("show_jellyfin_tab", false)
   val showQuickPlayFab = preferenceStore.getBoolean("show_quick_play_fab", true)
   val quickPlayFabDirect = preferenceStore.getBoolean("quick_play_fab_direct", false)
+  val showCelestialEffects = preferenceStore.getBoolean("show_celestial_effects", true)
 
   val topLeftControls =
     preferenceStore.getString(
